1abba754c0f17771a344de1f5f7f42ed4a57760a
Keiichi Sato <51529803+kei1-st@users.noreply.github.com>
2024-02-18 07:27:53 +0000

    reduce unnecessary rendering

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -1,4 +1,4 @@
-import { useState, memo } from 'react';
+import { useState, memo, useCallback } from 'react';
 
 import IconButton from '../UI/IconButton.jsx';
 import MinusIcon from '../UI/Icons/MinusIcon.jsx';
@@ -7,11 +7,7 @@ import CounterOutput from './CounterOutput.jsx';
 import { log } from '../../log.js';
 
 function isPrime(number) {
-  log(
-    'Calculating if is prime number',
-    2,
-    'other'
-  );
+  log('Calculating if is prime number', 2, 'other');
   if (number <= 1) {
     return false;
   }
@@ -27,23 +23,23 @@ function isPrime(number) {
   return true;
 }
 
-const Counter = memo( function Counter({ initialCount }) {
+const Counter = memo(function Counter({ initialCount }) {
   log('<Counter /> rendered', 1);
   const initialCountIsPrime = isPrime(initialCount);
 
   const [counter, setCounter] = useState(initialCount);
 
-  function handleDecrement() {
+  const handleDecrement = useCallback(function handleDecrement() {
     setCounter((prevCounter) => prevCounter - 1);
-  }
+  }, []);
 
-  function handleIncrement() {
+  const handleIncrement = useCallback(function handleIncrement() {
     setCounter((prevCounter) => prevCounter + 1);
-  }
+  }, []);
 
   return (
-    <section className="counter">
-      <p className="counter-info">
+    <section className='counter'>
+      <p className='counter-info'>
         The initial counter value was <strong>{initialCount}</strong>. It{' '}
         <strong>is {initialCountIsPrime ? 'a' : 'not a'}</strong> prime number.
       </p>
